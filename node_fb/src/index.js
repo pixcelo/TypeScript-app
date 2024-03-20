43928a9733a3f88bf9129d97ea2b148b2c01434c
pixcelo <bodomu@gmail.com>
2024-03-21 04:53:53 +0900

    mod: 静的ファイル表示

--- node_fb/src/index.js
+++ node_fb/src/index.js
@@ -4,10 +4,6 @@ const app = express();
 app.set('port', 3000);
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-});
-
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 });
